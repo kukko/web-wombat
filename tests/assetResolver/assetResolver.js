92de23467a920a563af4cf0c6b561e8ae81c7dc5
Nyárai Gergő <nyariag@gmail.com>
2019-03-05 14:27:10 +0100

    --fix: TESTS: Change assetResolver test to make a request which will return status code 404.

--- tests/assetResolver/assetResolver.js
+++ tests/assetResolver/assetResolver.js
@@ -1,27 +1,53 @@
-let { WombatServer } = require('../../index.js');
+let { WombatServer } = require("../../index.js");
 
 WombatServer.withoutDatabase()
 	.setUnsecure()
 	.setRoutes([])
-	.init((port) => {
-		require('http')
-			.get(
-				'http://localhost:' +
-					port +
-					'/resources/assets/images/wombat.jpg',
-				(response) => {
-					let data = '';
-					response.on('data', (chunk) => {
-						data += chunk;
-					});
-					response.on('end', () => {
-						console.log('Response: ' + data);
-						process.exit();
-					});
+	.init(port => {
+		let http = require("http"),
+			runnedTests = 0,
+			finishTest = () => {
+				runnedTests++;
+				if (runnedTests === 2) {
+					process.exit();
 				}
-			)
-			.on('error', (error) => {
-				console.log(error);
-				process.exit();
-			});
+			};
+		http.get(
+			"http://localhost:" + port + "/resources/assets/images/wombat.jpg",
+			response => {
+				if (response.statusCode !== 200) {
+					throw new Error("Not existing asset.");
+				}
+				let data = "";
+				response.on("data", chunk => {
+					data += chunk;
+				});
+				response.on("end", () => {
+					console.log("Existing asset request finished!");
+					finishTest();
+				});
+			}
+		).on("error", error => {
+			console.log(error);
+			process.exit(1);
+		});
+		http.get(
+			"http://localhost:" + port + "/resources/assets/images/missing.jpg",
+			response => {
+				if (response.statusCode !== 404) {
+					throw new Error("Request returned other code than 404.");
+				}
+				let data = "";
+				response.on("data", chunk => {
+					data += chunk;
+				});
+				response.on("end", () => {
+					console.log("Missing asset request finished!");
+					finishTest();
+				});
+			}
+		).on("error", error => {
+			console.log(error);
+			process.exit(1);
+		});
 	});
