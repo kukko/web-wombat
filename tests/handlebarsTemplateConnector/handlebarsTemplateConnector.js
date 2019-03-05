2e84c1c055795e1a38b7494f0c95e9d59e4a838b
Nyárai Gergő <nyariag@gmail.com>
2019-03-05 16:20:18 +0100

    --progress: TESTS: Test handlebars connector how it can return the expected output.

--- tests/handlebarsTemplateConnector/handlebarsTemplateConnector.js
+++ tests/handlebarsTemplateConnector/handlebarsTemplateConnector.js
@@ -1,14 +1,14 @@
-let { WombatServer, Route, templateConnectors } = require('../../index.js'),
-	HandlebarsController = require('./controllers/HandlebarsController/HandlebarsController.js');
+let { WombatServer, Route, templateConnectors } = require("../../index.js"),
+	HandlebarsController = require("./controllers/HandlebarsController/HandlebarsController.js");
 
 WombatServer.withoutDatabase()
 	.setRoutes([
-		Route.get('/existingView', HandlebarsController, 'existingView'),
-		Route.get('/notExistingView', HandlebarsController, 'notExistingView')
+		Route.post("/existingView", HandlebarsController, "existingView"),
+		Route.get("/notExistingView", HandlebarsController, "notExistingView")
 	])
 	.setTemplateConnector(templateConnectors.HandlebarsConnector)
 	.setUnsecure()
-	.init((port) => {
+	.init(port => {
 		let totalRequests = 2,
 			completedRequests = 0;
 		completeRequest = () => {
@@ -17,36 +17,58 @@ WombatServer.withoutDatabase()
 				process.exit();
 			}
 		};
-		require('http')
-			.get('http://localhost:' + port + '/existingView', (response) => {
-				let data = '';
-				response.on('data', (chunk) => {
-					data += chunk;
-				});
-				response.on('end', () => {
-					console.log('Existing view: ' + data);
-					completeRequest();
-				});
-			})
-			.on('error', (error) => {
-				console.log(error);
-				process.exit(1);
-			});
-		require('http')
-			.get(
-				'http://localhost:' + port + '/notExistingView',
-				(response) => {
-					let data = '';
-					response.on('data', (chunk) => {
+		let request = require("http")
+			.request(
+				{
+					host: "localhost",
+					port: port,
+					path: "/existingView",
+					method: "POST"
+				},
+				response => {
+					let data = "";
+					response.on("data", chunk => {
 						data += chunk;
 					});
-					response.on('end', () => {
-						console.log('Not existing view: ' + data);
+					response.on("end", () => {
+						if (data !== "<h1>" + sentString + "</h1>") {
+							throw new Error('Received response is not containing the sent string!');
+						}
 						completeRequest();
 					});
 				}
 			)
-			.on('error', (error) => {
+			.on("error", error => {
+				console.log(error);
+				process.exit(1);
+			});
+		let sentString = "";
+		for (let i = 0; i < 8; i++) {
+			let chars =
+				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			sentString += chars.charAt(
+				Math.floor(Math.random() * chars.length)
+			);
+		}
+		request.write("foo=" + sentString);
+		request.end();
+		require("http")
+			.get("http://localhost:" + port + "/notExistingView", response => {
+				if (response.statusCode !== 200){
+					throw new Error('Not existing route!');
+				}
+				let data = "";
+				response.on("data", chunk => {
+					data += chunk;
+				});
+				response.on("end", () => {
+					if (data !== 'VIEW ERROR!'){
+						throw new Error('Not existing view request returned with wrong response!');
+					}
+					completeRequest();
+				});
+			})
+			.on("error", error => {
 				console.log(error);
 				process.exit(1);
 			});
