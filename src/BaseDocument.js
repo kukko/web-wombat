bbd4daff1029543cbdd5177945784dafdc2a13e7
Nyárai Gergő <nyariag@gmail.com>
2019-03-12 20:29:25 +0100

    --progress: RESOURCE_CONTROLLER: Not setting values for document fields in static instances.

--- src/BaseDocument.js
+++ src/BaseDocument.js
@@ -1,15 +1,28 @@
 class BaseDocument {
 	static getStructure(values) {
-		if (typeof this.structure === 'undefined'){
+		if (typeof this.structure === "undefined") {
 			this.createStructure();
 		}
-		if (typeof values !== 'undefined'){
-			for (let fieldName in values){
-				let field = this.getFieldByName(fieldName);
-				if (typeof field !== 'undefined' && typeof values[fieldName] !== 'undefined'){
+		if (typeof values !== "undefined") {
+			let output = [];
+			for (let i in this.structure) {
+				output.push(
+					Object.assign(
+						new this.structure[i].constructor(),
+						this.structure[i]
+					)
+				);
+			}
+			for (let fieldName in values) {
+				let field = this.getFieldByName(fieldName, output);
+				if (
+					typeof field !== "undefined" &&
+					typeof values[fieldName] !== "undefined"
+				) {
 					field.setValue(values[fieldName]);
 				}
 			}
+			return output;
 		}
 		return this.structure;
 	}
@@ -17,25 +30,27 @@ class BaseDocument {
 		this.structure = [];
 		this.buildStructure();
 	}
-	static buildStructure(){
-	}
+	static buildStructure() {}
 	static addField(field) {
 		this.structure.push(field);
 		return this;
 	}
-	static getFieldByName(name){
-		for (let i in this.structure){
-			if (this.structure[i].name === name){
-				return this.structure[i];
+	static getFieldByName(name, fields) {
+		if (typeof fields === 'undefined'){
+			fields = this.structure;
+		}
+		for (let i in fields) {
+			if (fields[i].name === name) {
+				return fields[i];
 			}
 		}
 	}
-	static validateDocument(values){
+	static validateDocument(values) {
 		let fields = this.getStructure(values),
 			output = true;
-		for (let fieldIndex in fields){
+		for (let fieldIndex in fields) {
 			output = output && fields[fieldIndex].validate();
-			if (!output){
+			if (!output) {
 				break;
 			}
 		}
