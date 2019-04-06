a3b080530f77d1b0823a75b7669a0687a3ac654c
Nyárai Gergő <nyariag@gmail.com>
2019-04-06 13:52:01 +0200

    --fix: ROUTES: Export route service from module.

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ module.exports = {
 	ResourceController: require("./src/ResourceController.js"),
 	WebSocketController: require("./src/WebSocketController.js"),
 	Route: require("./src/Route.js"),
+	RouteService: require("./src/services/RouteService.js"),
 	BaseMiddleware: require("./src/middlewares/BaseMiddleware.js"),
 	middlewares,
 	BaseCollection: require("./src/BaseCollection.js"),
