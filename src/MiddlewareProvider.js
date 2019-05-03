56f5803cb8aa6c15bbb51d6f8f977e10cbd19743
Nyárai Gergő <nyariag@gmail.com>
2019-05-03 20:41:05 +0200

    --progress: MIDDLEWARES: Create method to get all middlewares from middleware provider.

--- src/MiddlewareProvider.js
+++ src/MiddlewareProvider.js
@@ -48,8 +48,45 @@ class MiddlewareProvider {
 			}
 		}
 	}
+	static getAllMiddlewares(reload = false){
+		if (typeof this.middlewares === "undefined" || reload){
+			this.loadMiddlewares();
+		}
+		return this.middlewares;
+	}
+	static loadMiddlewares(){
+		this.middlewares = {};
+		let { resolve, join } = require("path"),
+			{ readdirSync, lstatSync } = require("fs"),
+			middlewaresFolder = resolve(__dirname, "./middlewares"),
+			middlewareFolders = readdirSync(middlewaresFolder)
+				.map((folder) => {
+					return {
+						parentFolder: middlewaresFolder,
+						middlewareFolder: folder
+					};
+				})
+				.filter((source) => {
+					return lstatSync(
+						join(source.parentFolder, source.middlewareFolder)
+					).isDirectory();
+				})
+				.map((collection) => {
+					return collection.middlewareFolder;
+				});
+
+		for (let i in middlewareFolders) {
+			let middlewareName = middlewareFolders[i];
+			this.middlewares[middlewareName] = require(join(
+				middlewaresFolder,
+				middlewareName,
+				middlewareName + ".js"
+			));
+		}
+		return this;
+	}
 }
 
-MiddlewareProvider.middlewares = [];
+MiddlewareProvider.loadMiddlewares();
 
 module.exports = MiddlewareProvider;
