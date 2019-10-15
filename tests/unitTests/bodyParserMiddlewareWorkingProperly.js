6f00e533d118c1827f249df42c096333df232e9d
Nyárai Gergő <nyariag@gmail.com>
2019-10-15 16:56:41 +0200

    --progress: TESTS: Create tests for BodyParserMiddleware.

---
+++ tests/unitTests/bodyParserMiddlewareWorkingProperly.js
@@ -0,0 +1,56 @@
+let assert = require('chai').assert,
+	sinon = require('sinon');
+sinon.assert.expose(assert);
+
+describe('BodyParserMiddleware works expected', () => {
+    let BodyParserMiddleware,
+        request,
+        response,
+        next,
+        data = {
+            foo: 'bar'
+        };
+    before(() => {
+        BodyParserMiddleware = require('../../src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js');
+    });
+    beforeEach(() => {
+		request = {
+			method: "GET",
+			url: "",
+			upgrade: false,
+            headers: {},
+            rawBody: require('querystring').stringify(data)
+		};
+        response = {};
+        next = sinon.spy();
+    });
+    it('Not set body for get request', () => {
+        let BodyParserMiddleware = require('../../src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js');
+        BodyParserMiddleware.run(request, response, next);
+        assert.isUndefined(request.body);
+    });
+    it('Parse body for POST request', () => {
+        request.method = "POST";
+        BodyParserMiddleware.run(request, response, next);
+        assert.deepEqual(request.body, data);
+    });
+    it('Parse body for PUT request', () => {
+        request.method = "PUT";
+        BodyParserMiddleware.run(request, response, next);
+        assert.deepEqual(request.body, data);
+    });
+    it('Parse body for UPDATE request', () => {
+        request.method = "UPDATE";
+        BodyParserMiddleware.run(request, response, next);
+        assert.deepEqual(request.body, data);
+    });
+    it('Parse body for DELETE request', () => {
+        request.method = "DELETE";
+        BodyParserMiddleware.run(request, response, next);
+        assert.deepEqual(request.body, data);
+    });
+    it('Called next in every situation', () => {
+        BodyParserMiddleware.run(request, response, next);
+        sinon.assert.calledOnce(next);
+    });
+});
