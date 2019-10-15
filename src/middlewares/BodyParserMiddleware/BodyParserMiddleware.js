ee4d770d04c5153f4d3bff7414a1074d92232af9
Nyárai Gergő <nyariag@gmail.com>
2019-10-15 16:52:55 +0200

    --fix: MIDDLEWARES: Parse request body for DELETE requests.

--- src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js
+++ src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js
@@ -2,7 +2,7 @@ let BaseMiddleware = require("../BaseMiddleware.js");
 
 class BodyParserMiddleware extends BaseMiddleware {
 	static run(request, response, next) {
-		if (["POST", "PUT", "UPDATE"].indexOf(request.method) !== -1) {
+		if (["POST", "PUT", "UPDATE", "DELETE"].indexOf(request.method) !== -1) {
 			request.body = require("querystring").parse(request.rawBody);
 		}
 		next();
