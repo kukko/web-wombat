955cf02615dd365db7b863e4531d4dde602c3e68
Nyárai Gergő <nyariag@gmail.com>
2019-10-15 14:39:40 +0200

    --enhancement: MIDDLEWARES: Create middleware to redirect unauthorized requests.

---
+++ src/middlewares/RedirectUnauthorizedMiddleware/RedirectUnauthorizedMiddleware.js
@@ -0,0 +1,33 @@
+let BaseMiddleware = require("../BaseMiddleware.js");
+
+class RedirectUnauthorizedMiddleware extends BaseMiddleware {
+	static run(request, response, next, params) {
+		if (!request.authenticated){
+			let isRedirectURLSetted = typeof params.redirectURL !== "undefined",
+				isRedirectRouteSetted = typeof params.redirectRouteAlias !== "undefined";
+			if (!isRedirectURLSetted && !isRedirectRouteSetted){
+				response.statusCode = 302;
+				response.setHeader("Location", "/");
+				response.end();
+			}
+			if (isRedirectURLSetted){
+				response.statusCode = 302;
+				response.setHeader("Location", params.redirectURL);
+				response.end();
+			}
+			else if (isRedirectRouteSetted){
+				let RouteService = require("../../services/RouteService.js"),
+					redirectParameters = typeof params.redirectRouteParameters !== "undefined" ? params.redirectRouteParameters : {},
+					redirectURL = RouteService.getRouteByAlias(params.redirectRouteAlias, redirectParameters);
+				response.statusCode = 302;
+				response.setHeader("Location", redirectURL);
+				response.end();
+			}
+		}
+		else{
+			next();
+		}
+	}
+}
+
+module.exports = RedirectUnauthorizedMiddleware;
