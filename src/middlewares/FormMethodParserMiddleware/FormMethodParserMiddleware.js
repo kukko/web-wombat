e8df81290e929e2ee033a39d82970080c4d778bb
Nyárai Gergő <nyariag@gmail.com>
2019-10-16 23:18:12 +0200

    --fix: MIDDLEWARES: Handle cases when the request don't have a parsed body.

--- src/middlewares/FormMethodParserMiddleware/FormMethodParserMiddleware.js
+++ src/middlewares/FormMethodParserMiddleware/FormMethodParserMiddleware.js
@@ -4,6 +4,7 @@ class FormMethodParserMiddleware extends BaseMiddleware {
 	static run(request, response, next) {
 		if (
 			request.method === "POST" &&
+			typeof request.body !== "undefined" &&
 			typeof request.body._form_method !== "undefined"
 		) {
 			request.method = request.body._form_method;
