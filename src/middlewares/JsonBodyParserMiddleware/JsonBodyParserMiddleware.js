1db7c49f46069bf33b850593fe5c70ee6bcd4703
Nyárai Gergő <nyariag@gmail.com>
2020-10-23 23:56:47 +0200

    --fix: MIDDLEWARES: Change json body parser middleware.

--- src/middlewares/JsonBodyParserMiddleware/JsonBodyParserMiddleware.js
+++ src/middlewares/JsonBodyParserMiddleware/JsonBodyParserMiddleware.js
@@ -2,7 +2,7 @@ let BaseMiddleware = require("../BaseMiddleware.js");
 
 class JsonBodyParserMiddleware extends BaseMiddleware {
 	static run(request, response, next) {
-		if (["POST", "PUT", "UPDATE"].indexOf(request.method) !== -1 && request.headers['content-type'].indexOf('application/json;') !== -1) {
+		if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) !== -1 && request.headers['content-type'].indexOf('application/json') !== -1) {
 			request.body = JSON.parse(request.rawBody);
 		}
 		next();
