d6733b0cb7487380845439ffeb9c7d8ad826018d
Nyárai Gergő <nyariag@gmail.com>
2020-11-06 15:44:26 +0100

    --fix: MIDDLEWARES: Do not parse body if it is already parsed.

--- src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js
+++ src/middlewares/BodyParserMiddleware/BodyParserMiddleware.js
@@ -2,7 +2,7 @@ let BaseMiddleware = require("../BaseMiddleware.js");
 
 class BodyParserMiddleware extends BaseMiddleware {
 	static run(request, response, next) {
-		if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) !== -1) {
+		if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) !== -1 && typeof request.body === 'undefined') {
 			request.body = require("querystring").parse(request.rawBody);
 		}
 		next();
